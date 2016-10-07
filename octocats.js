92bb6abe7b30e6ba1d88d0f82d8697e50171f154
DanyloKuleshko <7dkuleshko@stpatrick.org>
2016-10-07 10:27:49 -0500

    Add files via upload
    created a new octocat

---
+++ octocats.js
@@ -0,0 +1,22 @@
+//Write your constructor function below.
+function octocat(name, arms){
+
+
+//You should have 2 instance variables and 1 instance function included.
+this.name=bydd;
+this.arms=arm;
+this.slap= function slap(){
+  for(arm=8;arm>0;arm--)
+  console.log(name+"just slapped the boop out of you. you will be boop slapped"+arm+"more times")
+}
+}
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
